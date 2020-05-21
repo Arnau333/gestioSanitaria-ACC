8c14f3918922ce641d80544fc763454d1bc488d8
Arnau <arnaucarrasco@ginebro.cat>
2020-05-21 16:38:41 +0200

    NEW ingressarPacients

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict'
 
+function validaQueNoEsBuit(cadenaAValidar) {
+      if (cadenaAValidar.value=="" || cadenaAValidar.value=='undefined') {
+        mostraMissatge(`El camp <b>${cadenaAValidar.name}</b> no pot quedar-se en blanc!`);
+
+      } 
+    }
+    
+
 const eleID_divEspaiModal = document.getElementById("divEspaiModal");
 
 const eleID_barra_missatges = document.getElementById("divBarraMissatges");
@@ -23,13 +31,10 @@ const eleID_h2ResutltatFormControls = document.getElementById("h2ResutltatFormCo
 /*
 taula_missatges[0][intTitol]="No habilitat"
 taula_missatges[0][intMissatge]="Ho sento, encara no està habilitat!"
-
 taula_missatges[1][intTitol]="Camp buit"
 taula_missatges[1][intMissatge]="Cal que entris un nombre al camp màxim pacients!"
-
 taula_missatges[2][intTitol]="Camp buit"
 taula_missatges[2][intMissatge]="Cal que entris un nom al camp Hospital!"
-
 taula_missatges[3][intTitol]="Camp buit"
 taula_missatges[3][intMissatge]="Ho sento, encara no està habilitat!"
  */
@@ -82,9 +87,9 @@ function comprovaCampNumero(objecteRebut) {
 
 function mostraMissatge(codiMissatge) {
       /* Amb format JQuery   */
-      $('#divEspaiModal').find('#titolModal').text(taula_missatges[codiMissatge][intTitol]);
-      $('#divEspaiModal').find('#missatgeModal').text(taula_missatges[codiMissatge][intMissatge]);
-      $('#divEspaiModal').modal('show');
+      // $('#divEspaiModal').find('#titolModal').text(taula_missatges[codiMissatge][intTitol]);
+      // $('#divEspaiModal').find('#missatgeModal').text(taula_missatges[codiMissatge][intMissatge]);
+      // $('#divEspaiModal').modal('show');
 }
 
 function mostraBotons() {
@@ -136,17 +141,10 @@ function amagaBotons() {
 
 /* https://developer.mozilla.org/en-US/docs/Web/API/Element/classList 
 document.getElementById("MyElement").classList.add('MyClass');
-
 document.getElementById("MyElement").classList.remove('MyClass');
-
 if ( document.getElementById("MyElement").classList.contains('MyClass') )
-
 document.getElementById("MyElement").classList.toggle('MyClass');
-
-
-
 https://stackoverflow.com/questions/195951/how-can-i-change-an-elements-class-with-javascript
-
 */
 
 function conteClass(element, nomClass) {
@@ -209,13 +207,53 @@ function ocultaGestioHospital(objecteRebut){
             mostraBotons();
    }
 
-function mostraGestioPacients(objecteRebut){
+function mostraGestioPacients(num){
       if (!conteClass(eleID_divPacient,"d-none")) {
             afegirClass(eleID_divPacient,"d-none");
       }
       eleID_divPresentacio.classList.toggle("d-none");
       eleID_divPacient.classList.toggle("d-none");
       amagaBotons();
+      var pacientsHTML="";
+
+      for (let index = 0; index < num; index++) {
+            pacientsHTML = '<div class="row">'+
+            '<div class="col mb-3">'+
+                '<label for="nomPacient'+(index)+'" class="font-weight-bold">Nom pacient:</label>'+
+                '<input type="text" title="Entra el nom del pacient!" name="Nom Pacient" onblur="validaQueNoEsBuit(this)" placeholder="Nom pacient" id="nomPacient'+(index)+'" class="form-control" required="" minlength="1" maxlength="100">'+
+            '</div>'+
+            '<div class="col mb-3">'+
+                    '<label for="nomMalaltiaPacient'+(index)+'" class="font-weight-bold">'+
+                        'Cognom pacient:'+
+                    '</label>'+
+                    '<input type="text" title="Entra el Cognom del pacient!" name="Cognom pacient:" onblur="validaQueNoEsBuit(this)" placeholder="Cognom pacient" id="Cognompacient'+(index)+'" class="form-control" required="">'+
+            '</div>'+
+            '<div class="col mb-3">'+
+                    '<label for="nomMalaltiaPacient'+(index)+'" class="font-weight-bold">'+
+                        'NIF de pacient:'+
+                    '</label>'+
+                    '<input type="text" title="Entra el NIF del pacient!" name="NIF pacient:" onblur="validaQueNoEsBuit(this)" placeholder="NIF pacient" id="NIF'+(index)+'" class="form-control" required="">'+
+            
+            '</div>'+
+            '<div class="col mb-3">'+
+                    '<label for="nomMalaltiaPacient'+(index)+'" class="font-weight-bold">'+
+                        'Malaltia de pacient:'+
+                    '</label>'+
+                    '<input type="text" title="Entra el nom del pacient!" name="Malaltia pacient:" onblur="validaQueNoEsBuit(this)" placeholder="Malaltia pacient" id="nomPacient'+(index)+'" class="form-control" required="">'+
+            
+            '</div>'+
+            '</div>';
+        
+            if(index==0){
+                  var htmlP = pacientsHTML.toString();
+            }else{
+                  htmlP += pacientsHTML.toString();
+            }
+
+      }
+      document.getElementById('dadesPacient').innerHTML =  htmlP;
+      // document.getElementById('test').innerHTML =  htmlP;
+
 }
 
 function ocultaGestioPacients(objecteRebut){
@@ -230,19 +268,39 @@ function ocultaGestioPacients(objecteRebut){
       mostraBotons();
    }
    
-   function crearHospital() {// s'ha ha de crar l'hospital....
+function crearHospital() {// s'ha ha de crar l'hospital....
       // alert('aaa');
-var nom = document.getElementById("inputNomHospital").value.toString();
-var maximPacients = parseInt(document.getElementById("maximPacientsHospital").value);
-   console.log(nom);
-   console.log(maximPacients);
+      var nom = document.getElementById("inputNomHospital").value.toString();
+      var maximPacients = parseInt(document.getElementById("maximPacientsHospital").value);
+      console.log(nom);
+      console.log(maximPacients);
 
    
-  if (nom !== "" && maximPacients > 0) {
-      
-      var hospital = new Hospital(nom, maximPacients);
+      if (nom !== "" && maximPacients > 0) {
+            
+            var hospital = new Hospital(nom, maximPacients);
 
-      ocultaGestioHospital();
-      mostraGestioPacients();
-  }
-   } 
+            ocultaGestioHospital();
+            mostraGestioPacients(maximPacients);
+
+       
+      }
+} 
+
+function ingressarPacients() {
+
+      var formularu = ["nomPacient", "Cognompacient", "NIF"];
+      for (let camp = 0; camp < formularu.length; camp++) {
+            //Nom
+            var index = 0;
+            var dadesPacientArray = [];
+            while(document.getElementById(formularu[camp]+(index))){
+                  dadesPacientArray[formularu[camp]+(index)]=document.getElementById(formularu[camp]+(index)).value;
+                  //comprovacions
+                  console.log(formularu[camp]+": "+dadesPacientArray[formularu[camp]+(index++)]);
+            }
+
+      }
+      
+
+}
